ef8544ac75eb2544940cc11215a53fca671349b5
lucascortez-dev <lucas.ac2001@gmail.com>
2026-09-16 23:25:10 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -438,7 +438,7 @@ elif menu == "Controle de Estoque":
 # -------------------------------------------------------------
 # ABA: INTEGRAÇÃO MERCADO LIVRE
 # -------------------------------------------------------------
-elif "Mercado" in menu:
+elif menu == "Integracao ML":
     st.title("Integração Oficial - Mercado Livre")
     
     # Pega as chaves do cofre do Streamlit
